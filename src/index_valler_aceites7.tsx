d9bba212c9e30382f22d4b98efe43b48a59fab7f
BagasyStudio <bagasystudio@gmail.com>
2026-07-19 22:54:40 -0300

    register rosemary avatar composition in farm entry

--- src/index_valler_aceites7.tsx
+++ src/index_valler_aceites7.tsx
@@ -4,14 +4,28 @@ import {
   MainVallerAceites7,
   TOTAL_FRAMES_VALLER_ACEITES7,
 } from "./VideoEdit/Main_valler_aceites7";
+import {
+  MainVbb0rdkrfduo,
+  TOTAL_FRAMES_VBB0RDKRFDUO,
+} from "./VideoEdit/Main_vbb0rdkrfduo";
 
 export const VallerAceites7Root: React.FC = () => (
-  <Composition
-    id="VallerAceites7"
-    component={MainVallerAceites7}
-    durationInFrames={TOTAL_FRAMES_VALLER_ACEITES7}
-    fps={30}
-    width={1920}
-    height={1080}
-  />
+  <>
+    <Composition
+      id="Vbb0rdkrfduo"
+      component={MainVbb0rdkrfduo}
+      durationInFrames={TOTAL_FRAMES_VBB0RDKRFDUO}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+    <Composition
+      id="VallerAceites7"
+      component={MainVallerAceites7}
+      durationInFrames={TOTAL_FRAMES_VALLER_ACEITES7}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+  </>
 );
